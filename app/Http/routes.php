9188a0608fe9218d9a52957fde5f991f7e030887
EC2 Default User <ec2-user@ip-172-31-14-134.us-east-2.compute.internal>
2018-03-04 09:53:18 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
